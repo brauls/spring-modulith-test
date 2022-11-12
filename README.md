95b9774b7596e8b5bd509d1724ddce617b3a041f
Benedikt Rauls <brauls101@gmail.com>
2022-11-12 17:12:55 +0100

    init commit test application

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # spring-modulith-test
+
+## Links
+
+* [VW Model Configurator](https://www.volkswagen.de/de/modelle.html)
+* [Domain Modeling Tool](https://www.wps.de/modeler/index.html)
+* [Domain Modeling Example](https://leasingninja.io/)
---
+++ src/main/java/de/brauls/demo/SpringModulithTestApplication.java
@@ -0,0 +1,13 @@
+package de.brauls.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringModulithTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringModulithTestApplication.class, args);
+	}
+
+}
